e6a1a229d66259cf22a64d9948ec5732ac1ea40d
Alireza <mdinvarzade@gmail.com>
2022-12-05 11:36:30 +0330

    add information of tenant to request

--- django/request/serializer.py
+++ django/request/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Request
 from property.serializer import PropertySerializer
+from account.serializers import RetrieveUpdateDestroyUserSerializer
+from account.models import User
 
 
 class CreateRequestSerializer(serializers.ModelSerializer):
@@ -12,10 +14,15 @@ class CreateRequestSerializer(serializers.ModelSerializer):
 
 class RequestSerializer(serializers.ModelSerializer):
     request_property = PropertySerializer(read_only=True)
+    tenant = serializers.SerializerMethodField()
 
     class Meta:
         model = Request
-        exclude = ('tenant',)
+        fields = "__all__"
+
+    def get_tenant(self, obj):
+        tenant = User.objects.get(id=obj.tenant_id)
+        return RetrieveUpdateDestroyUserSerializer(instance=tenant).data
 
 
 class LandlordUpdateRequestSerializer(serializers.ModelSerializer):
