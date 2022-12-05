7aa76198be8366ccd14f785915eedd7564ca29f0
Alireza <mdinvarzade@gmail.com>
2022-12-05 11:54:45 +0330

    add user_addtional_information to contract request

--- django/contract/serializer.py
+++ django/contract/serializer.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
 from account.serializers import RetrieveUpdateDestroyUserSerializer
+from account.serializers import CreateAdditionalUserInformationSerializer
 from .models import Contract
-from account.models import User
+from account.models import User, UserAdditionalInformation
 
 
 class ContractSerializer(serializers.ModelSerializer):
     contract_landlord = serializers.SerializerMethodField()
+    contract_landlord_information = serializers.SerializerMethodField()
     contract_tenant = serializers.SerializerMethodField()
+    contract_tenant_information = serializers.SerializerMethodField()
 
     class Meta:
         model = Contract
@@ -17,10 +20,18 @@ class ContractSerializer(serializers.ModelSerializer):
         contract_landlord = User.objects.get(id=obj.contract_landlord_id)
         return RetrieveUpdateDestroyUserSerializer(instance=contract_landlord).data
 
+    def get_contract_landlord_information(self, obj):
+        contract_landlord_information = UserAdditionalInformation.objects.get(user_id=obj.contract_landlord_id)
+        return CreateAdditionalUserInformationSerializer(instance=contract_landlord_information).data
+
 
     def get_contract_tenant(self, obj):
         contract_tenant = User.objects.get(id=obj.contract_tenant_id)
         return RetrieveUpdateDestroyUserSerializer(instance=contract_tenant).data
+
+    def get_contract_tenant_information(self, obj):
+        contract_tenant_information = UserAdditionalInformation.objects.get(user_id=obj.contract_tenant_id)
+        return CreateAdditionalUserInformationSerializer(instance=contract_tenant_information).data
 
 
     def validate(self, attrs):
