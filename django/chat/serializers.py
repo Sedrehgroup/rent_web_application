ef7f89cc337216d42f130636ef1cc5f45bb4ea40
Alireza <mdinvarzade@gmail.com>
2022-12-20 18:10:18 +0330

    (ShayestehHS) Customize the ListCreateChatSerializer.[publisher, tenant]

--- django/chat/serializers.py
+++ django/chat/serializers.py
@@ -5,14 +5,18 @@ from chat.models import Note, Chat
 
 class ListCreateChatSerializer(serializers.ModelSerializer):
     property_title = serializers.SerializerMethodField(read_only=True)
-    publisher_fullname = serializers.SerializerMethodField(read_only=True)
+    publisher = serializers.SerializerMethodField(read_only=True)
+    tenant = serializers.SerializerMethodField(read_only=True)
     last_text = serializers.SerializerMethodField(read_only=True)
 
     def get_property_title(self, obj: Chat):
         return obj.property.title
 
-    def get_publisher_fullname(self, obj: Chat):
-        return obj.publisher.get_full_name()
+    def get_tenant(self,obj:Chat):
+        return {"id": obj.tenant_id,"full_name":obj.tenant.get_full_name()}
+
+    def get_publisher(self, obj: Chat):
+        return {"id": obj.publisher_id,"full_name":obj.publisher.get_full_name()}
 
     def get_last_text(self, obj: Chat):
         last_note = Note.objects.filter(chat=obj).order_by("-created_date").only("text").first()
@@ -20,13 +24,13 @@ class ListCreateChatSerializer(serializers.ModelSerializer):
 
     def validate(self, attr):
         if attr["tenant"].id == attr["property"].owner_id:
-            raise ValidationError({"tenant":"tenant ID is equal to publisher ID of property"})
+            raise ValidationError({"tenant": "tenant ID is equal to publisher ID of property"})
         return attr
 
     class Meta:
         model = Chat
         fields = ["id", "tenant", "property", "publisher",
-                  "property_title", "publisher_fullname", "last_text"]
+                  "property_title", "publisher", "last_text"]
 
 
 class ListCreateNotesByChatSerializer(serializers.ModelSerializer):
