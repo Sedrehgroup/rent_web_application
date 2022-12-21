b21dd36fa531a6c97e72d9edf261f2e6888993b7
shayestehhs <shayesthhs1@gmail.com>
2022-12-21 12:18:25 +0330

    Fix the create chat serializer

--- django/chat/serializers.py
+++ django/chat/serializers.py
@@ -1,22 +1,29 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from chat.models import Note, Chat
+from account.models import User
 
 
 class ListCreateChatSerializer(serializers.ModelSerializer):
     property_title = serializers.SerializerMethodField(read_only=True)
-    publisher = serializers.SerializerMethodField(read_only=True)
-    tenant = serializers.SerializerMethodField(read_only=True)
     last_text = serializers.SerializerMethodField(read_only=True)
+
+    def to_representation(self, instance):
+        """Convert `username` to lowercase."""
+        ret = super().to_representation(instance)
+        publisher= User.objects.get(id=ret["publisher"])
+        tenant = User.objects.get(id=ret["tenant"])
+        ret["publisher"] = {"id": publisher.id, "full_name": publisher.get_full_name()}
+        ret["tenant"] = {"id": tenant.id, "full_name": tenant.get_full_name()}
+        return ret
+
+    def validate(self, attr):
+        if attr["tenant"].id == attr["property"].owner_id:
+            raise ValidationError({"tenant":"tenant ID is equal to publisher ID of property"})
+        return attr
 
     def get_property_title(self, obj: Chat):
         return obj.property.title
-
-    def get_tenant(self,obj:Chat):
-        return {"id": obj.tenant_id,"full_name":obj.tenant.get_full_name()}
-
-    def get_publisher(self, obj: Chat):
-        return {"id": obj.publisher_id,"full_name":obj.publisher.get_full_name()}
 
     def get_last_text(self, obj: Chat):
         last_note = Note.objects.filter(chat=obj).order_by("-created_date").only("text").first()
